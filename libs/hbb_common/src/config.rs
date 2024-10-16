98e191f5f0b3420c7d15bc80b92772b31aaf1bd2
MoonDropwzy <93063707+MoonDropwzy@users.noreply.github.com>
2024-10-16 13:59:00 +0800

    Update config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const PUBLIC_RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["61.171.69.243"];
+pub const PUBLIC_RS_PUB_KEY: &str = "mm6rUrkwLndqWsuNh0a7VKIKG3VPDTEDCI6NdjlZP2k=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
